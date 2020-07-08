19e7570c3583e075f79ef8e5186417d71510e626
Maryam Sohrabi <sohrabm@ucalgary.ca>
2020-07-08 18:35:42 +0000

    added ephemeris loading methods and conjunctions file

---
+++ aurorax/conjunctions.py
@@ -0,0 +1,16 @@
+import json
+import requests
+import datetime
+
+
+def search(start_datetime, end_datetime, conjunction_type="nbtrace", source_list=None, distance_list=None, metadata_filters=None):
+    # check for valid start and end datetimes
+    
+    # check for valid conjunction type
+    
+    # check for valid objects in source list
+    
+    # check for valid distances
+    
+    # check for valid metadata filters 
+    pass
---
+++ aurorax/ephemeris.py
@@ -0,0 +1,211 @@
+import json
+import pytz
+import aacgmv2
+import requests
+import datetime
+
+API_EPHEMERIS_SEARCH_URL = "http://api.staging.aurorax.space:8080/api/v1/ephemeris/search"
+API_EPHEMERIS_SOURCES_URL = "http://api.staging.aurorax.space:8080/api/v1/ephemeris-sources"
+API_EPHEMERIS_UPLOAD_URL = "http://api.staging.aurorax.space:8080/api/v1/ephemeris-sources/{}/ephemeris"
+API_KEY = None
+
+
+def get_ephemeris_sources(program=None, platform=None, instrument_type=None, source_type=None, owner=None, format="basic_info"):
+    """
+    Returns a list of dictionaries representing all ephemeris sources.
+    """
+    filters = {"program": program, "platform": platform, "instrument_type": instrument_type, "source_type": source_type, "owner": owner, "format": format}
+    r = requests.get(API_EPHEMERIS_SOURCES_URL, params=filters)
+    ephemeris_response = eval(r.text)
+    
+    return ephemeris_response
+
+
+def set_api_key(api_key):
+    global API_KEY
+    API_KEY = api_key
+
+
+def search(programs=None, platforms=None, instrument_types=None, metadata_filters=None, start_datetime=None, end_datetime=None):
+    """
+    Search for ephemeris records
+    """
+    # check that both start and end datetimes were provided
+    if start_datetime and end_datetime:
+        print("Start and end datetimes are valid")
+    else:
+        print("No start and/or end datetime was specified")
+    # check that at least one filter parameter is provided
+    if not all(p is None for p in [programs, platforms, instrument_types, source_metadata_filters, ephemeris_metadata_filters]):
+        print("At least one parameter was specified")
+    
+    pass
+    
+    
+def __process_record(data, metadata, source_id):
+    ephemeris_record = {
+                        "ephemeris_source_identifier": None,
+                        "program": None,
+                        "platform": None,
+                        "instrument_type": None,
+                        "epoch": None,
+                        "location_geo": {
+                            "lat": None,
+                            "lon": None
+                            },
+                        "location_gsm": {
+                            "lat": None,
+                            "lon": None,
+                            },
+                        "nbtrace": {
+                            "lat": None,
+                            "lon": None
+                            },
+                        "sbtrace": {
+                            "lat": None,
+                            "lon": None
+                            },
+                        "metadata": metadata
+                    }
+    
+    try:
+        # check for valid data variables (program, platform, instrument_type, UTC date&time, latitude, longitude)
+        ephemeris_record["ephemeris_source_identifier"] = source_id
+        ephemeris_record["program"] = data["program"]
+        ephemeris_record["platform"] = data["platform"]
+        ephemeris_record["instrument_type"] = data["instrument_type"]
+        
+        try:
+            record_epoch = datetime.datetime(data["year"], data["month"], data["day"], data["hour"], data["minute"], 0, tzinfo=pytz.utc)
+            ephemeris_record["epoch"] = record_epoch.isoformat()
+        except Exception as de:
+            print(de)
+            return -1
+        
+        if not (data["geo_lat"] >= -90 and data["geo_lat"] <= 90 and data["geo_lon"] >= -180 and data["geo_lon"] <= 180):
+            print("Invalid GEO location parameter. Try again.")
+            return -1
+        else:
+            ephemeris_record["location_geo"]["lat"] = data["geo_lat"]
+            ephemeris_record["location_geo"]["lon"] = data["geo_lon"]
+            
+        # calculate AACGM coordinates
+        if data["geo_lat"] >= 0:
+            # northern hemisphere
+            record_nbtrace_location_mag = aacgmv2.convert_latlon(data["location_geo"]["lat"], data["location_geo"]["lon"], 0.0, data["epoch"])
+                    
+            # flip the geomagnetic latitude and convert back to geographic coordinates for the south B trace
+            record_sbtrace_location_geo = aacgmv2.convert_latlon(record_nbtrace_location_mag[0] * -1.0, record_nbtrace_location_mag[1], 0.0, data["epoch"], method_code="A2G")
+        else:
+            # southern hemisphere
+            record_sbtrace_location_mag = aacgmv2.convert_latlon(data["location_geo"]["lat"], data["location_geo"]["lon"], 0.0, data["epoch"])
+                    
+            # flip the geomagnetic latitude and convert back to geographic coordinates for the north B trace
+            record_nbtrace_location_geo = aacgmv2.convert_latlon(record_sbtrace_location_mag[0] * -1.0, record_sbtrace_location_mag[1], 0.0, data["epoch"], method_code="A2G")
+        
+        ephemeris_record["nbtrace"]["lat"] = record_nbtrace_location_mag[0]
+        ephemeris_record["nbtrace"]["lon"] = record_nbtrace_location_mag[1]
+        ephemeris_record["sbtrace"]["lat"] = record_sbtrace_location_mag[0]
+        ephemeris_record["sbtrace"]["lon"] = record_sbtrace_location_mag[1]
+    except Exception as e:
+        print(e)
+        return -1
+    
+    return ephemeris_record
+
+    
+def upload(data, metadata=[]):
+    """
+    Upload a single ephemeris record to AuroraX.
+    """
+    # get the ephemeris source ID
+    ids = get_ephemeris_sources()
+    source_id = None
+    
+    for source in ids:
+        if source["program"] == data["program"] and source["platform"] == data["platform"] and source["instrument_type"] == data["instrument_type"]:
+            source_id = source["identifier"]
+            break
+    if source_id is None:
+        print("No ephemeris source was found to match this program, platform and instrument type. Please check the data parameters and try again.")
+        return -1
+    
+    record = __process_record(date, metadata, source_id)
+
+    if record != -1:
+        r = requests.post(API_EPHEMERIS_UPLOAD_URL.format(source_id), headers={"accept": "application/json", "Content-Type": "application/json", "x-aurorax-api-key": API_KEY}, json=ephemeris_record)
+        return r
+
+
+def upload_many(data_list, metadata_list):
+    """
+    Upload many ephemeris records to AuroraX.
+    """
+    if len(data_list) != len(metadata_list):
+        print("The number of data records does not match the number of metadata records. Try again.")
+        return -1
+    elif not isinstance(data_list, list) or not isinstance(metadata_list, list):
+        print("Data and metadata parameters must be lists. Try again.")
+        return -1
+    
+    ids = get_ephemeris_sources()
+    all_records = {}
+    
+    try:
+        for i in data_list:
+            for source in ids:
+                if source["program"] == data["program"] and source["platform"] == data["platform"] and source["instrument_type"] == data["instrument_type"]:
+                    source_id = source["identifier"]
+                    break
+            if source_id is None:
+                print("No ephemeris source was found to match program, platform and instrument type at data list index " + str(i) + ". Please check the data parameters and try again.")
+                return -1
+            
+            data = data_list[i]
+            metadata = metadata_list[i]
+            
+            record = __process_record(data, metadata, source_id)
+            if record != -1:
+                all_records[source_id] = record
+            else:
+                print("Error encountered at data list index " + str(i) + ". Try again.")
+                return -1
+            
+        for id in all_records:
+            r = requests.post(API_EPHEMERIS_UPLOAD_URL.format(id), headers={"accept": "application/json", "Content-Type": "application/json", "x-aurorax-api-key": API_KEY}, json=all_records[id])
+            if r.status_code != 202:
+                print("Error occurred while sending data to the API for source ID " + str(id) + ".\nAborting.")
+                return r
+    except Exception as e:
+        print(e)
+        return -1
+
+
+class Ground:
+    """
+    Ground(program, platform, instrument_type[, id])
+    
+    Represents a ground-based ephemeris source.
+    """
+
+    def __init__(self, program=None, platform=None, instrument_type=None, id=None):
+        # add checks for validity
+        self.program = program
+        self.platform = platform
+        self.instrument_type = instrument_type
+        self.id = id
+        
+
+class Space:
+    """
+    Space(program, platform, instrument_type[, id])
+    
+    Represents a space-based ephemeris source.
+    """
+
+    def __init__(self, program=None, platform=None, instrument_type=None, id=None):
+        # add checks for validity
+        self.program = program
+        self.platform = platform
+        self.instrument_type = instrument_type
+        self.id = id
