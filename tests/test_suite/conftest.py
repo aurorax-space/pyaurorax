7b487b8f0c0ae7a2eae95ffafc71ea01fea616d5
Maryam Sohrabi <sohrabi.mar@gmail.com>
2021-06-16 15:04:09 -0600

    Added local env test option with host

--- tests/test_suite/conftest.py
+++ tests/test_suite/conftest.py
@@ -4,25 +4,40 @@ import pytest
 import sys
 
 def pytest_addoption(parser):
-    parser.addoption("--env", action="store", default="staging", help="test environment: staging or production")
+    parser.addoption("--env", action="store", default="staging", help="Test environment: 'local', 'staging', or 'production'. Defaults to 'staging'.")
+    parser.addoption("--host", action="store", default="http://localhost:3000", help="Local API host address, defaults to localhost:3000 if --env=local")
 
 
 @pytest.fixture(scope="session")
 def env(request):
     return request.config.getoption("--env")
 
+@pytest.fixture(scope="session")
+def host(request):
+    return request.config.getoption("--host")
 
 @pytest.fixture(scope="session", autouse=True)
-def set_env_api_key(env):
-    if env == "staging":
-        print("\n\nTest environment: STAGING")
+def set_env_api_key(env, host):
+    url = ""
+    print(f"\n\nTest environment: {env}")
+
+    if env == "local":
+        url = host
+        aurorax.api.authenticate(os.getenv("AURORAX_APIKEY_LOCAL"))
+
+    elif env == "staging":
+        url = "https://api.staging.aurorax.space"
         aurorax.api.authenticate(os.getenv("AURORAX_APIKEY_STAGING"))
-        if aurorax.get_api_key() == "":
-            print("API key not found")
-        aurorax.api.set_base_url("https://api.staging.aurorax.space")
+        
     elif env == "production":
-        print("\n\nTest environment: PRODUCTION")
+        url = "https://api.aurorax.space"
         aurorax.api.authenticate(os.getenv("AURORAX_APIKEY_PRODUCTION"))
-        aurorax.api.set_base_url("https://api.aurorax.space")
     else:
+        print(f"Error: env input {env} not recognized")
         sys.exit(-1)
+
+    if not aurorax.get_api_key():
+        print(f"Warning: {env} API key not found")
+
+    print("Using base address: " + url)
+    aurorax.api.set_base_url(url)
