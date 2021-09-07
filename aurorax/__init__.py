88dae95f8b2040d9039aa4377499e8530a8d9e2c
Maryam Sohrabi <sohrabm@ucalgary.ca>
2021-09-07 10:35:17 -0600

    Bump version to 0.6.5

--- aurorax/__init__.py
+++ aurorax/__init__.py
@@ -1,7 +1,7 @@
 """
 PyAuroraX package.
 """
-__version__ = "0.6.4"
+__version__ = "0.6.5"
 
 # pull in top level functions
 from .api import AuroraXRequest
--- tests/test_suite/test_version.py
+++ tests/test_suite/test_version.py
@@ -2,4 +2,4 @@ from aurorax import __version__
 
 
 def test_version():
-    assert __version__ == "0.6.4"
+    assert __version__ == "0.6.5"
