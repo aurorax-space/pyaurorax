f2cf09bc24630a7538206ab164e08f2d1aeb1240
Maryam Sohrabi <sohrabm@ucalgary.ca>
2021-11-05 09:43:34 -0600

    Bump version to 0.6.10

--- aurorax/__init__.py
+++ aurorax/__init__.py
@@ -22,7 +22,7 @@ Basic usage:
 > import aurorax
 ```
 """
-__version__ = "0.6.9"
+__version__ = "0.6.10"
 
 # pull in top level functions
 from .api import AuroraXRequest
--- tests/test_suite/test_version.py
+++ tests/test_suite/test_version.py
@@ -2,4 +2,4 @@ from aurorax import __version__
 
 
 def test_version():
-    assert __version__ == "0.6.9"
+    assert __version__ == "0.6.10"
