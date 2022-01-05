8e210b25cfa5164630082e15497cb914b5d9db97
Darren Chaddock <dchaddoc@ucalgary.ca>
2022-01-05 21:03:44 +0000

    added choices to the pytest conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,10 @@ import sys
 
 def pytest_addoption(parser):
     parser.addoption("--env", action="store", default="staging",
-                     help="Test environment: 'local', 'staging', or 'production', defaults to 'staging'")
+                     choices=["local", "staging", "production"],
+                     help="Test server to use. If set to staging or production, "
+                     "the corresponding URL for that server will be used. Also the "
+                     "environment variable with the corresponding suffix will be used.")
     parser.addoption("--host", action="store", default="http://localhost:3000",
                      help="Local API host address, defaults to localhost:3000 if --env=local")
 
@@ -23,9 +26,11 @@ def host(request):
 
 @pytest.fixture(scope="session", autouse=True)
 def set_env_api_key(env, host):
+    # init
     url = ""
     print(f"\n\nTest environment: {env}")
 
+    # set url and authenticate
     if env == "local":
         url = host
         pyaurorax.api.authenticate(os.getenv("AURORAX_APIKEY_LOCAL"))
@@ -39,8 +44,10 @@ def set_env_api_key(env, host):
         print(f"Error: env input {env} not recognized")
         sys.exit(-1)
 
+    # check to make sure an API key was set
     if not pyaurorax.get_api_key():
         print(f"Warning: {env} API key not found")
 
+    # set base URL
     print("Using base address: " + url)
     pyaurorax.api.set_base_url(url)
