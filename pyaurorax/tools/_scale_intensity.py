5df1e241e576532ab75ce8d52647297b61f12c33
Darren Chaddock <dchaddoc@ucalgary.ca>
2024-06-19 17:36:06 +0000

    added scaling flag, added atm notebooks

--- pyaurorax/tools/_scale_intensity.py
+++ pyaurorax/tools/_scale_intensity.py
@@ -16,33 +16,7 @@ import numpy as np
 from typing import Optional
 
 
-def scale_intensity(
-    data: np.ndarray,
-    min: Optional[float] = None,
-    max: Optional[float] = None,
-    top: Optional[float] = None,
-) -> np.ndarray:
-    """
-    Scale all values of an array that lie in the range min<=x<=max in to 
-    the range 0<=x<=high.
-    
-    Args:
-        data (numpy.ndarray): data array, can be 2, 3, or 4-dimensional. Assumed 
-            to be an image, or array of images. Also assumed that the first 2 dimensions
-            are the image's x and y coordinates, and the following dimensions are some
-            combination of the number of images, and/or the colour channel.
-        min (float): minimum value of array to be considered
-        max (float): maximum value of array to be considered
-        top (float): maximum value of the scaled result. If not supplied, the max value
-            of the data array's dtype is used.
-
-    Returns:
-        A new `numpy.ndarray` that is the same dimensions as the inputted data array, 
-        with the scaling applied.
-
-    Raises:
-        ValueError: Issues with the supplied min, max, or top
-    """
+def __scale_data(data, min, max, top):
     # init
     bottom = 0
 
@@ -88,3 +62,72 @@ def scale_intensity(
 
     # return
     return scaled_data
+
+
+def scale_intensity(
+    data: np.ndarray,
+    min: Optional[float] = None,
+    max: Optional[float] = None,
+    top: Optional[float] = None,
+    memory_saver: bool = False,
+) -> np.ndarray:
+    """
+    Scale all values of an array that lie in the range min<=x<=max in to 
+    the range 0<=x<=high.
+    
+    Args:
+        data (numpy.ndarray): 
+            Data array, can be 2, 3, or 4-dimensional. Assumed to be an image, or array of 
+            images. Also assumed that the first 2 dimensions are the image's x and y 
+            coordinates, and the following dimensions are some combination of the number of 
+            images, and/or the colour channel.
+
+        min (float): 
+            Minimum value of array to be considered
+
+        max (float): 
+            Maximum value of array to be considered
+
+        top (float): 
+            Maximum value of the scaled result. If not supplied, the max value
+            of the data array's dtype is used.
+
+        memory_saver (bool): 
+            Utilize less RAM when scaling a set of images. Note that this routine is slower if this
+            is set to `True`.
+
+    Returns:
+        A new `numpy.ndarray` that is the same dimensions as the inputted data array, 
+        with the scaling applied.
+
+    Raises:
+        ValueError: Issues with the supplied min, max, or top
+    """
+    if (memory_saver is True):
+        # determine if we are single or 3 channel
+        n_channels = 1
+        if (len(data.shape) == 3):
+            # single channel
+            n_channels = 1
+        elif (len(data.shape) == 4):
+            # three channel
+            n_channels = 3
+        else:
+            ValueError("Unable to determine number of channels based on the supplied images. Make sure you are supplying a " +
+                       "[rows,cols,images] or [rows,cols,channels,images] sized array.")
+
+        # init destination array
+        images_scaled = np.empty((data.shape), dtype=data.dtype)
+
+        # cycle through each image
+        for i in range(0, data.shape[-1]):
+            if (n_channels == 1):
+                images_scaled[:, :, i] = __scale_data(data[:, :, i], min, max, top)
+            else:
+                images_scaled[:, :, :, i] = __scale_data(data[:, :, :, i], min, max, top)
+
+        # return
+        return images_scaled
+    else:
+        # scale and return
+        return __scale_data(data, min, max, top)
