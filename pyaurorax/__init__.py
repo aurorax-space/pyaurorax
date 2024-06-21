845729cc8e43fa531974b879439805602e558648
Darren Chaddock <dchaddoc@ucalgary.ca>
2024-06-21 01:35:56 +0000

    bumped version

--- pyaurorax/__init__.py
+++ pyaurorax/__init__.py
@@ -32,7 +32,7 @@ aurorax = pyaurorax.PyAuroraX()
 """
 
 # versioning info
-__version__ = "1.0.0-rc3"
+__version__ = "1.0.0-rc4"
 
 # documentation excludes
 __pdoc__ = {"cli": False, "pyaurorax": False}
