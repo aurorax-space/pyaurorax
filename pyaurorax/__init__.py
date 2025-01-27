130e4cb9c6dc7269cc9f8c3268182c98f577fd25
Darren Chaddock <dchaddoc@ucalgary.ca>
2025-01-27 14:14:18 +0000

    bump version

--- pyaurorax/__init__.py
+++ pyaurorax/__init__.py
@@ -32,7 +32,7 @@ aurorax = pyaurorax.PyAuroraX()
 """
 
 # versioning info
-__version__ = "1.11.0"
+__version__ = "1.12.0"
 
 # documentation excludes
 __pdoc__ = {"cli": False, "pyaurorax": False}
--- tests/test_suite/test_version.py
+++ tests/test_suite/test_version.py
@@ -16,4 +16,4 @@ from pyaurorax import __version__
 
 
 def test_version():
-    assert __version__ == "1.11.0"
+    assert __version__ == "1.12.0"
