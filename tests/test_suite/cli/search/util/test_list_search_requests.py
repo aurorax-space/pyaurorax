d61baae3937871c9eef3e9f0f25cc1d8ef588263
Darren Chaddock <dchaddoc@ucalgary.ca>
2025-02-04 14:53:09 -0700

    updated tests

--- tests/test_suite/cli/search/util/test_list_search_requests.py
+++ tests/test_suite/cli/search/util/test_list_search_requests.py
@@ -1,3 +1,17 @@
+# Copyright 2024 University of Calgary
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
 from pyaurorax.cli.cli import cli
 
@@ -18,6 +32,6 @@ def test_simple(cli_runner, api_key):
 
 @pytest.mark.cli
 def test_active(cli_runner, api_key):
-    result = cli_runner.invoke(cli, ("--api-base-url=https://api.staging.aurorax.space --api-key=%s " +
-                                     "search util list_search_requests --active") % (api_key))
+    result = cli_runner.invoke(cli, ("--api-base-url=https://api.staging.aurorax.space --api-key=%s search util list_search_requests --active") %
+                               (api_key))
     assert result.exit_code == 0
